d61b2b7292ff607e3143e5025ae9b39a40b5a5b6
lalagulaxmed96 <tu6g4w77k@code.edu.az>
2024-05-07 13:10:45 +0400

    qwerty

---
+++ WebApplication5/WebApplication5/Controllers/Work-singleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    public class Work_singleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
